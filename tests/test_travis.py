f16d0eb75087b7f50ab1db1bcc8aa252e0719154
1 <ppark9553@gmail.com>
2018-07-30 14:10:16 +0900

    travis 테스트

---
+++ tests/test_travis.py
@@ -0,0 +1,21 @@
+'''
+Mined.
+
+ ʕ•ﻌ•ʔ     ʕ•ﻌ•ʔ
+( >  <) ♡ (>  < )
+ u   u     u   u
+
+feat. peepee
+with love...
+'''
+import pytest
+
+
+## Test 시작 ##
+@pytest.fixture
+def example_data():
+    data = 'data from fixture'
+    return data
+
+def test_travis_works(example_data):
+    assert example_data == 'data from fixture'
