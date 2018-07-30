2c1195241f455e63f1014537523cb6a5e99e5092
1 <ppark9553@gmail.com>
2018-07-30 13:25:29 +0900

    DS_Store 제거 그리고 예시 view 추가

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class TestAPIView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        result = {'status': 'GOOD'}
+        return Response(result, status=200)
