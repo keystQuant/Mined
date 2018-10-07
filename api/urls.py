ff235bf8c0cc41553723feb30f31996c3bf023a4
1 <ppark9553@gmail.com>
2018-10-07 12:55:59 +0900

    url 추가

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.views import TaskAPIView, TestAPIView
+from api.views import GatewayView, TaskAPIView, TestAPIView
 
 urlpatterns = [
     path('test/', TestAPIView.as_view(), name='test'),
     path('', TaskAPIView.as_view(), name='task'),
+    path('task/', GatewayView.as_view(), name='test'),
 ]
